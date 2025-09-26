711e76c8d98645feb056fc4cf46d06ef1c14569c
Egor <139051554+EgorGalkinORG@users.noreply.github.com>
2025-09-26 11:09:55 +0300

    added /posts/:id page, and skip take functional

--- index.js
+++ index.js
@@ -13,9 +13,39 @@ app.get('/timestamp', (req, res) => {
 
 app.get('/posts', (req, res) => {
     const dataPath = path.join(__dirname, 'posts.json');
-    res.json(
-        JSON.parse(fs.readFileSync(dataPath, 'utf-8'))
-    );
+    let posts = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+    let skipNum = 0;
+    let takeNum = posts.length;
+
+    if (req.query.skip !== undefined) {
+        skipNum = Number(req.query.skip);
+    }
+
+    if (req.query.take !== undefined) {
+        takeNum = Number(req.query.take);
+    }
+
+    posts = posts.slice(skipNum, skipNum + takeNum);
+
+    res.json(posts);
+    
+});
+
+app.get('/posts/:id', (req, res) => {
+    const dataPath = path.join(__dirname, 'posts.json');
+    let posts = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+    const id = +req.params.id;
+
+    // решил с помощью post =>
+    const post = posts.find(post => id === id);
+
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+
+    res.json(post);
 });
 
 app.listen(5000, () => {
